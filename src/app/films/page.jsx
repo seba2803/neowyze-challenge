a48eb8dbfe3fd00283d023c0c1735b53c674c95e
seba2803 <sebaovelazquez@gmail.com>
2024-05-09 20:47:34 -0300

    add(-): add background, title of landing page, routes films and people, files services and utils, responsive config in file tailwind.config.js.

---
+++ src/app/films/page.jsx
@@ -0,0 +1,3 @@
+export default function Films() {
+  return <h1>ruta films</h1>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,72 +1,9 @@
-import Image from 'next/image';
+import Landing from '@/pages/Landing';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      {/* <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-        <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
-          <Image
-            src='/vercel.svg'
-            alt='Vercel Logo'
-            className='dark:invert'
-            width={100}
-            height={24}
-            priority
-          />
-        </div>
-      </div>
-      <Image
-        className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-        src='/next.svg'
-        alt='Next.js Logo'
-        width={180}
-        height={37}
-        priority
-      />
-
-      <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
-        <a
-          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-            -&gt;
-          </span>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-          -&gt;
-        </span>
-
-        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          Learn about Next.js in an interactive course with&nbsp;quizzes!
-        </p>
-
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-        <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-          Instantly deploy your Next.js site to a shareable URL with Vercel.
-        </p>
-      </div> */}
+    <main>
+      <Landing />
     </main>
   );
 }
