8a90d632a0814e115c03d206f05dacfe6eaa81c6
seba2803 <sebaovelazquez@gmail.com>
2024-05-09 20:38:14 -0300

    add(-): create Landing page

---
+++ src/pages/Landing.js
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import logoStarWars from '../../public/star-wars-logo.jpg';
+import Link from 'next/link';
+
+export default function Landing() {
+  return (
+    <div className='grid place-items-center'>
+      <Image
+        src={logoStarWars}
+        alt='logo star-wars'
+        width={500}
+        height={300}
+        placeholder='blur'
+      />
+      <div className='text-center flex flex-wrap flex-col place-items-center border-2 border-white rounded-lg w-4/5 phone:w-full tablet:w-3/4 h-24 m-8 bg-slate-900'>
+        <h2 className='text-xl mt-1'>
+          View about to <i>films</i>
+        </h2>
+        <Link
+          href={'/films'}
+          className='border-2 border-white rounded-md px-3 hover:bg-stone-700 transition-all mt-5 text-lg w-3/5'
+        >
+          <button>
+            <strong>Here</strong>
+          </button>
+        </Link>
+      </div>
+      <div className='text-center flex flex-wrap flex-col place-items-center border-2 border-white rounded-lg w-4/5 phone:w-full tablet:w-3/4 h-24 m-8 bg-slate-900'>
+        <h2 className='text-xl mt-1'>
+          View about to <i>people</i>
+        </h2>
+        <Link
+          href={'/people'}
+          className='border-2 border-white rounded-md px-3 hover:bg-stone-700 transition-all mt-5 text-lg w-3/5'
+        >
+          <button>
+            <strong>Here</strong>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
