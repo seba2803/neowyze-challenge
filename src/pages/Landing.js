a16898e9da537b45e20f0f037a8cfeded8e995ab
seba2803 <sebaovelazquez@gmail.com>
2024-05-09 21:52:46 -0300

    add(-): adding especifications about next components

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,17 +5,13 @@ import Link from 'next/link';
 export default function Landing() {
   return (
     <div className='grid place-items-center'>
-      <Image
-        src={logoStarWars}
-        alt='logo star-wars'
-        width={500}
-        height={300}
-        placeholder='blur'
-      />
+      {/*se utiliza el componente Image para un mejor renderizado de imagen*/}
+      <Image src={logoStarWars} alt='logo star-wars' width={500} height={300} />
       <div className='text-center flex flex-wrap flex-col place-items-center border-2 border-white rounded-lg w-4/5 phone:w-full tablet:w-3/4 h-24 m-8 bg-slate-900'>
         <h2 className='text-xl mt-1'>
           View about to <i>films</i>
         </h2>
+        {/*boton que redirige a ruta films y a su vez evita que la pagina se recargue al viajar por distintas rutas*/}
         <Link
           href={'/films'}
           className='border-2 border-white rounded-md px-3 hover:bg-stone-700 transition-all mt-5 text-lg w-3/5'
@@ -29,6 +25,7 @@ export default function Landing() {
         <h2 className='text-xl mt-1'>
           View about to <i>people</i>
         </h2>
+        {/*boton que redirige a ruta people y a su vez evita que la pagina se recargue al viajar por distintas rutas*/}
         <Link
           href={'/people'}
           className='border-2 border-white rounded-md px-3 hover:bg-stone-700 transition-all mt-5 text-lg w-3/5'
