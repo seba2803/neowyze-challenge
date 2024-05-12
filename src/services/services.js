7b2da4190bd6be67bdaf477d4a779d9c32a02c53
seba2803 <sebaovelazquez@gmail.com>
2024-05-11 21:53:00 -0300

    add(-): create method for fetching people data in services.js

--- src/services/services.js
+++ src/services/services.js
@@ -46,21 +46,58 @@ export const getFilm = async (id) => {
   }
 };
 
-//* limpia el estado Film
-export const clearFilm = () => {
-  state.Film = {};
-};
-
 //* obtiene todas las personas
 export const getPeople = async () => {
   try {
-  } catch (error) {}
+    //* arreglo de promesas sin resolver
+    const promisePeople = [];
+    for (let index = 1; index < 10; index++) {
+      //* pusheo en promisePeople las promesas sin resolver que hacen el fetching de datos a ruta people
+      promisePeople.push(
+        fetchingData(`https://swapi.dev/api/people/?page=${index}`)
+      );
+    }
+    //* resuleve todas las promesas pendientes de promisePeople usando Promise all
+    Promise.all(promisePeople).then((value) => {
+      //? cargo todos los datos en el estado People
+      state.People = value.flat();
+    });
+  } catch (error) {
+    console.log({ error: error.message });
+  }
 };
+
+//* funcion que hace fetching de ruta people
+async function fetchingData(url) {
+  const result = await fetch(url);
+  const data = await result.json();
+  //* formateo la data para un mejor uso de la información
+  const formatData = data.results.map(
+    ({
+      name,
+      height,
+      mass,
+      hair_color,
+      skin_color,
+      eye_color,
+      birth_year,
+      gender,
+    }) => ({
+      nombre: name,
+      altura: height,
+      masa: mass,
+      color_pelo: hair_color,
+      color_piel: skin_color,
+      color_ojos: eye_color,
+      compleano: birth_year,
+      genero: gender,
+    })
+  );
+  return formatData;
+}
 
 //* otiene la persona para verla a detalle
 export const getPerson = async () => {
   try {
   } catch (error) {}
 };
-
-//* limpia el estado Person
