dbb47a74d86677c6ff947fc34e2a4f241f26223e
seba2803 <sebaovelazquez@gmail.com>
2024-05-13 16:58:26 -0300

    refactor(-): change name to utils function and change structure of getPeople and fetchingData function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,13 @@
 import { state } from '@/services/services';
-
-export function showState(people) {
+//? extrae todas las personas que aparecen en el Film
+export function getPeopleFromFilmg(people) {
+  //* si en el estado Film posee la propiedad personajes(un Array) que lo mapee
   if (state.Film.personajes) {
+    //* extraigo los indices de las url de los personajes
     const indexes = state.Film.personajes.map((url) => {
+      //* parseo a entero el resultado para tener indices enteros
+      //* a la url la convierto en un array de todos los caracteres que posee la url
+      //* filtro de ese array todos los numeros(indices)
       return parseInt(
         url
           .split('')
@@ -10,13 +15,13 @@ export function showState(people) {
           .join('')
       );
     });
-    console.log('indices', indexes);
-    console.log('local storage', people);
     const peopleFromFilm = [];
+    //* mientras el array de indices tenga elementos
+    //* voy a llenar a peopleFromFilm de todos los objetos que hay en el array people
+    //* que concuerden con el indice que se extrajo de las url
     while (indexes.length) {
       peopleFromFilm.push(people[indexes.pop()]);
     }
-    console.log('personas de cada film', peopleFromFilm);
     return peopleFromFilm;
   }
 }
